ca30e99c9a6ac9a34ee736b32c1135ca4356bfa9
Fabio <Fabio@192.168.1.4>
2021-02-27 16:33:07 -0300

    adição do swagger

--- prova/src/main/java/br/com/tree/prova/config/SwaggerConfig.java
+++ prova/src/main/java/br/com/tree/prova/config/SwaggerConfig.java
@@ -2,8 +2,10 @@ package br.com.tree.prova.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,35 +14,35 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Component
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig extends WebMvcConfigurationSupport {
+public class SwaggerConfig implements WebMvcConfigurer {
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo());
+    }
 
-  @Bean
-  public Docket greetingApi() {
-    return new Docket(DocumentationType.SWAGGER_2)
-        .select()
-        .apis(RequestHandlerSelectors.any())
-        .paths(PathSelectors.any())
-        .build()
-        .apiInfo(metaData());
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Custom Title")
+                .description("Custom Description")
+                .version("1.0.0")
+                .build();
+    }
 
-  }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
 
-  private ApiInfo metaData() {
-    return new ApiInfoBuilder()
-        .title("Prova Tecnica Tree Tecnologia")
-        .description("\"Prova Tecnica Tree Tecnologia\"")
-        .version("1.0.0")
-        .build();
-  }
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 
-  @Override
-  protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("swagger-ui.html")
-        .addResourceLocations("classpath:/META-INF/resources/");
-
-    registry.addResourceHandler("/webjars/**")
-        .addResourceLocations("classpath:/META-INF/resources/webjars/");
-  }
 }
